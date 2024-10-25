6ff96e5053699a23df26a759aa52e21a4e29c502
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2024-10-25 15:58:01 +0545

    updating ui

--- BOUTIQUE/urls.py
+++ BOUTIQUE/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from BOUTIQUE import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('boutique/', views.boutique, name='boutique'),
-    path('search/', views.search, name='search'),
-    path('boutique_post/<str:slug>/', views.boutique_post, name='boutique_post'),
-    path('contact/', views.contact, name='contact'),
-    path('order/', views.order_view, name='order'),
     path('new_arrivals/', views.new_arrivals_view, name='new_arrivals'),
-    path('product_detail/<int:sno>/', views.product_detail, name='product_detail'),
+    path('boutique_post/<slug:slug>/', views.boutique_post, name='boutique_post'),
+    path('product_detail/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('order/', views.order_view, name='order'),
+    path('order/<int:product_id>/', views.order_view, name='order'),
     path('submit_order/', views.submit_order, name='submit_order'),
+    path('search/', views.search, name='search'),
+    path('contact/', views.contact, name='contact'),
 ]
