d6507295bf2be621894d352491e2a3e612ac6cb0
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2025-02-15 14:07:04 +0545

    update

--- KRITI_BOUTIQUE/settings.py
+++ KRITI_BOUTIQUE/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-%2s3)m1v_t%@oc3q8=lfqw&#*%me$vonatw9%v=qezag2tbnzr
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['.vercel.app']
 
 
