ecfcda8405925a02030d8dcd8417d7542b942fd3
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2026-01-02 13:53:55 +0545

    deployment

--- KRITI_BOUTIQUE/settings.py
+++ KRITI_BOUTIQUE/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-%2s3)m1v_t%@oc3q8=lfqw&#*%me$vonatw9%v=qezag2tbnzr
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['KRITI_BOUTIQUE-lynn.onrender.com', 'localhost']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
